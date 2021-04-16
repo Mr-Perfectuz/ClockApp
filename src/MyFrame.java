661c772e5e16ccfab5aec223c38d73f02da20b68
Mr-Perfectuz <ziyodovanvar1999@gmail.com>
2021-04-16 12:24:37 +0900

    JFrame has been implemented

--- src/MyFrame.java
+++ src/MyFrame.java
@@ -1,3 +1,21 @@
-public class MyFrame {
+import javax.swing.*;
+import java.awt.*;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+public class MyFrame extends JFrame {
+
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+    JLabel timeLabel;
+
+    MyFrame(){
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Electronic clock");
+        this.setLayout(new FlowLayout());
+        this.setSize(350, 200);
+        this.setResizable(false);
+        this.setVisible(true);
+
+    }
 }
